eadbb77aad700f5ca7054303ee01741e89d18681
Erik Johansson <erik@trailingbits.com>
2024-06-04 15:04:16 +0200

    improvement - if all pre-requisites are met, the buildall.bat should result in a dist folder with all executables

--- buildall.bat
+++ buildall.bat
@@ -1,6 +1,23 @@
 @echo off
+
+echo Building UI
+call :buildUI
+
+echo Building all executables for: %GOOS%, arch: %GOARCH%
 call :treeProcess
+
+echo Copying executables to dist folder dist\%GOOS%
+call :makeDist
 goto :eof
+
+:buildUI
+cd ui
+call npm i 
+call npm run build 
+rmdir /q /s ..\svcs\dd-ui\web\static
+xcopy /e /y /q dist\* ..\svcs\dd-ui\web\static\
+cd ..
+exit /b
 
 :treeProcess
 rem Do whatever you want here over the files of this subdir, for example:
@@ -16,3 +33,49 @@ for /D %%d in (*) do (
     )
 )
 exit /b
+
+:makeDist
+if "%GOOS%" == "" set GOOS=windows
+if "%GOOS%" == "windows" (
+    rmdir /q /s .\dist\%GOOS%
+    xcopy /q /y .\inner\dd-nats-cache\*.exe .\dist\%GOOS%\inner\
+    xcopy /q /y .\inner\dd-nats-file-inner\*.exe .\dist\%GOOS%\inner\
+    xcopy /q /y .\inner\dd-nats-inner-proxy\*.exe .\dist\%GOOS%\inner\
+    xcopy /q /y .\inner\dd-nats-modbus\*.exe .\dist\%GOOS%\inner\
+    xcopy /q /y .\inner\dd-nats-opcda\*.exe .\dist\%GOOS%\inner\
+    xcopy /q /y .\tools\sniffer\*.exe .\dist\%GOOS%\inner\
+    xcopy /q /y .\svcs\dd-logger\*.exe .\dist\%GOOS%\inner\
+    xcopy /q /y .\svcs\dd-ui\*.exe .\dist\%GOOS%\inner\
+    xcopy /q /y .\outer\dd-nats-file-outer\*.exe .\dist\%GOOS%\outer\
+    xcopy /q /y .\outer\dd-nats-influxdb\*.exe .\dist\%GOOS%\outer\
+    xcopy /q /y .\outer\dd-nats-outer-proxy\*.exe .\dist\%GOOS%\outer\
+    xcopy /q /y .\outer\dd-nats-process-filter\*.exe .\dist\%GOOS%\outer\
+    xcopy /q /y .\outer\dd-nats-rabbitmq\*.exe .\dist\%GOOS%\outer\
+    xcopy /q /y .\outer\dd-nats-timescale\*.exe .\dist\%GOOS%\outer\
+    xcopy /q /y .\tools\sniffer\*.exe .\dist\%GOOS%\outer\
+    xcopy /q /y .\svcs\dd-logger\*.exe .\dist\%GOOS%\outer\
+    xcopy /q /y .\svcs\dd-ui\*.exe .\dist\%GOOS%\outer\
+)
+
+if "%GOOS%" == "linux" (
+    rmdir /q /s .\dist\%GOOS%
+    xcopy /q /y .\inner\dd-nats-cache\dd-nats-cache .\dist\%GOOS%\inner\
+    xcopy /q /y .\inner\dd-nats-file-inner\dd-nats-file-inner .\dist\%GOOS%\inner\
+    xcopy /q /y .\inner\dd-nats-inner-proxy\dd-nats-inner-proxy .\dist\%GOOS%\inner\
+    xcopy /q /y .\inner\dd-nats-modbus\dd-nats-modbus .\dist\%GOOS%\inner\
+    xcopy /q /y .\tools\sniffer\sniffer .\dist\%GOOS%\inner\
+    xcopy /q /y .\svcs\dd-logger\dd-logger .\dist\%GOOS%\inner\
+    xcopy /q /y .\svcs\dd-ui\dd-ui .\dist\%GOOS%\inner\
+    xcopy /q /y .\outer\dd-nats-file-outer\dd-nats-file-outer .\dist\%GOOS%\outer\
+    xcopy /q /y .\outer\dd-nats-influxdb\dd-nats-influxdb .\dist\%GOOS%\outer\
+    xcopy /q /y .\outer\dd-nats-outer-proxy\dd-nats-outer-proxy .\dist\%GOOS%\outer\
+    xcopy /q /y .\outer\dd-nats-process-filter\dd-nats-process-filter .\dist\%GOOS%\outer\
+    xcopy /q /y .\outer\dd-nats-rabbitmq\dd-nats-rabbitmq .\dist\%GOOS%\outer\
+    xcopy /q /y .\outer\dd-nats-timescale\dd-nats-timescale .\dist\%GOOS%\outer\
+    xcopy /q /y .\tools\sniffer\sniffer .\dist\%GOOS%\outer\
+    xcopy /q /y .\svcs\dd-logger\dd-logger .\dist\%GOOS%\outer\
+    xcopy /q /y .\svcs\dd-ui\dd-ui .\dist\%GOOS%\outer\
+)
+exit /b
+
+:eof
